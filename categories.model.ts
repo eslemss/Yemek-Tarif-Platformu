5132c502e733a9c8e5d9e961ab1f4f2a11247000
Ayşe Eslem Gökhan <gokhanayse5@gmail.com>
2026-04-24 04:27:33 +0300

    Yemek Tarif

--- categories.model.ts
+++
@@ -1,8 +0,0 @@
-export class Category {
-  constructor(
-    public id?: any,
-    public name?: string,
-    public description?: string, // '?' ile opsiyonel yaptık
-    public imageUrl?: string     
-  ) {}
-} 
